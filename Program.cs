37a251b80b0f54a40caaf70a5c379e9ffcc2bdda
Guest 01 <guest01@alunos.sc.senac.br>
2022-05-05 20:14:57 -0300

    Identificador de ano bissexto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ano_bissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano = 0;
+            Console.WriteLine("Digite o Ano:");
+            ano = Int32.Parse(Console.ReadLine());
+
+            if (((ano%4==0) && (ano%100 != 0)) || (ano%400==0))
+            {
+                Console.WriteLine(" Este ano "+ ano +" é bissexto !!!!" );
+            }
+            else
+            {
+                Console.WriteLine("Este ano "+ ano + " não é bissexto");
+            }
+        }
+    }
+}
